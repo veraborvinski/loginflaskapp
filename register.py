710acebf3c21571c93e85f9ec08db66fd17c3821
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2024-01-15 12:33:46 +0000

    Update register.py

--- register.py
+++ register.py
@@ -48,4 +48,4 @@ def register():
     return render_template('register.html', error=error)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port="8080")
+    app.run(host='0.0.0.0',port="5000")
